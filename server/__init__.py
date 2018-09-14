51b89c576d60536cb20cb7c0ea80423025e8ad90
Juulsgaard <frederik@festen.dk>
2018-09-14 15:36:37 +0200

    minor import changes

--- server/__init__.py
+++ server/__init__.py
@@ -3,9 +3,8 @@ from flask import Flask
 from .endpoints.authentication import bp as auth
 from .endpoints.calendar import bp as cal
 
-import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append("..")
 
 
 def create_app():
